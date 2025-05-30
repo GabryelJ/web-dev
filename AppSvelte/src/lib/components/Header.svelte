afcf2647befbf53de76fcf40639c53d0bd157976
Gabryel <gabryeljuliods@gmail.com>
2025-05-30 20:04:30 -0300

    Componente Header

---
+++ AppSvelte/src/lib/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<header class="header">
+    <img class="logo" src="./images/logo.svg" alt="Logotipo smile">
+    <span class="span">Happy Face Corp.</span>
+</header>
+
+<style>
+    @import '../variables.css';
+    .header {
+        width: 100%;
+        height: 70px;
+        display: flex;
+        background-color: var(--primary-color);
+        align-items: center;
+    }
+    .span {
+        color: var(--secondary-color);
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .logo{
+        margin-left: 20px;
+        margin-right: 20px;
+        width: 45px;
+        height: 45px;
+    }
+</style>
